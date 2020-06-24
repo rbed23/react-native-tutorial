2ad8cbfb48731cd70b8f207e81052dc1387b6b16
Ryan B <rbed23@gmail.com>
2020-06-24 18:56:52 +0300

    update flatlist keys; reworked react-native @ package.json

--- screens/home.js
+++ screens/home.js
@@ -6,11 +6,11 @@ import Card from '../shared/card';
 export default function Home( props ) {
 
     const [reviews, setReviews] = useState([
-        { title: 'test1', rating: 5, body: 'lorm ipsum', id: '1'},
-        { title: 'test2', rating: 4, body: 'lorm ipsum', id: '2'},
-        { title: 'test3', rating: 3, body: 'lorm ipsum', id: '3'},
-        { title: 'test4', rating: 2, body: 'lorm ipsum', id: '4'},
-        { title: 'test5', rating: 1, body: 'lorm ipsum', id: '5'},
+        { title: 'test1', rating: 5, body: 'lorm ipsum', key: '1'},
+        { title: 'test2', rating: 4, body: 'lorm ipsum', key: '2'},
+        { title: 'test3', rating: 3, body: 'lorm ipsum', key: '3'},
+        { title: 'test4', rating: 2, body: 'lorm ipsum', key: '4'},
+        { title: 'test5', rating: 1, body: 'lorm ipsum', key: '5'},
     ]);
 
 
